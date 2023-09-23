36674a1bf2a47e28fa72e9edd22c4af39e56d154
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-09-23 20:50:51 +0530

    Update AStar_Path_Finder.cpp

--- AStar_Path_Finder.cpp
+++ AStar_Path_Finder.cpp
@@ -17,13 +17,13 @@ private:
 
 	struct sNode
 	{
-		bool bObstacle = false;			// Is the node an obstruction?
-		bool bVisited = false;			// Have we searched this node before?
+		bool bObstacle = false;			
+		bool bVisited = false;			
 		float fGlobalGoal;				// Distance to goal so far
 		float fLocalGoal;				// Distance to goal if we took the alternative route
-		int x;							// Nodes position in 2D space
+		int x;						// Nodes position in 2D space
 		int y;
-		vector<sNode*> vecNeighbours;	// Connections to neighbours
+		vector<sNode*> vecNeighbours;			// Connections to neighbours
 		sNode* parent;					// Node connecting to this node that offers shortest parent
 	};
 
